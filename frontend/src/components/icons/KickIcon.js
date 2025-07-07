853200dd3933bd6e254b834937f7ad024bca449a
EmeraldTort <jeffplays2009@gmail.com>
2025-07-07 18:51:14 +0100

    Build: Apply latest authentication and UI fixes

--- frontend/src/components/icons/KickIcon.js
+++ frontend/src/components/icons/KickIcon.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
-const KickIcon = ({ className }) => (
+const KickIcon = ({
+  size = 24,
+  color = 'currentColor',
+  className,
+  'aria-label': ariaLabel = 'Kick'
+}) => (
   <svg
     role="img"
+    aria-label={ariaLabel}
     viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    fill={color}
     xmlns="http://www.w3.org/2000/svg"
     className={className}
-    fill="currentColor"
   >
     <title>Kick</title>
+    {/* Left vertical bar */}
     <path
+      d="M2 3.857h6.143v16.293H2V3.857Z"
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M22 3.857V20.15H17.714V14.436L12 20.15H9.857L15.571 12L9.857 3.857H12L17.714 9.571V3.857H22ZM8.143 3.857V20.15H2V3.857H8.143Z"
+    />
+    {/* K‑shaped arrow */}
+    <path
+      d="M22 3.857v16.293h-4.286v-5.714L12 20.15h-2.143l5.714-8.15-5.714-8.15H12l5.714 5.714V3.857H22Z"
+      fillRule="evenodd"
+      clipRule="evenodd"
     />
   </svg>
 );
