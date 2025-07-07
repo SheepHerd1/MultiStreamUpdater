3a852e7667175ad6bd1b3f153ca0bf2576622345
EmeraldTort <jeffplays2009@gmail.com>
2025-07-07 19:05:14 +0100

    Build: Apply latest authentication and UI fixes

--- frontend/src/components/icons/KickIcon.js
+++ frontend/src/components/icons/KickIcon.js
@@ -8,9 +8,9 @@ const KickIcon = ({
   ...props
 }) => (
   <svg
-    viewBox="0 0 250 250"
+    viewBox="0 0 933 300"
     width={size}
-    height={size}
+    height={(size * 300) / 933}
     fill={color}
     role="img"
     aria-label={ariaLabel}
@@ -18,7 +18,7 @@ const KickIcon = ({
     className={className}
     {...props}
   >
-    <path d="M229.3 26.9L138 107.8v-57H113v135h25v-68l91.3 80.9h25v-96H250V0h-20.7z" />
+    <path d="M0 0h140.6v300H0zM198.8 0h59.5l119.6 255c-9.3 9.6-28.6 13.6-56 13.6H157.7V0zm329.6 0h45.8v231.6c0 26 9.3 39 27.9 39s27.9-13 27.9-39V0h45.8v233.9c0 35.1-17.8 52.7-53.5 52.7s-53.4-17.6-53.4-52.7V0zm297.1 0h45.8v188.5c0 26 9.3 39 27.9 39s27.9-13 27.9-39V0h45.8v255c-36.8 0-54.2-17.6-54.2-52.7V0z" />
   </svg>
 );
 
